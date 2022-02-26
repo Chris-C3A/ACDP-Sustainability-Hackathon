d6e69eeae568a104ae20467e178be9439b4f0b7f
u1trabot <juicyninja11@gmail.com>
2022-02-26 12:40:07 +0000

    Stock Images + CSS

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ main = Blueprint('main', __name__)
 
 @main.route('/')  # home page that return 'index'
 def index():
-    posts=[(1,"Title1",'/static/avacad.jpg','LOL xD'),(2,"Title2",'/static/avacad.jpg', 'Funny Caption'),(3,"Title3",'/static/avacad.jpg', 'haha'),(4,"Title4",'/static/avacad.jpg','WasteWatchers Unite!')] #map(tuple,fetchall(QUERY))
+    posts=[(1,"My Friday",'/static/stock1.jpg','LOL xD'),(2,"Your Monday",'/static/stock2.jpg', 'Funny Caption'),(3,"For the gram!",'/static/stock3.jpg', 'haha'),(4,"Honestly",'/static/stock4.jpg','WasteWatchers Unite!')] #map(tuple,fetchall(QUERY))
     page = render_template('postGen.html',posts=posts)
     return page
 
