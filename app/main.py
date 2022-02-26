1c6ed8c1391596c6ae1be5501452e8d62af55152
u1trabot <juicyninja11@gmail.com>
2022-02-26 12:05:07 +0000

    Fix Post Page

--- app/main.py
+++ app/main.py
@@ -5,12 +5,9 @@ main = Blueprint('main', __name__)
 
 
 @main.route('/')  # home page that return 'index'
-def posts():
+def index():
     posts=[(1,"Title1",'/static/avacad.jpg','LOL xD'),(2,"Title2",'/static/avacad.jpg', 'Funny Caption'),(3,"Title3",'/static/avacad.jpg', 'haha'),(4,"Title4",'/static/avacad.jpg','WasteWatchers Unite!')] #map(tuple,fetchall(QUERY))
-    page = render_template('top.html')
-    for post in posts:
-        page += render_template('postGen.html', postID=post[0], title=post[1], imgUrl=post[2], caption=post[3])
-    page += render_template('bottom.html')
+    page = render_template('postGen.html',posts=posts)
     return page
 
 
