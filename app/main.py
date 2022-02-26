73a09e8fde2834c9fad441030ed8c54b4a2776d4
c3a <anon_c3a@outlook.com>
2022-02-26 18:18:01 +0000

    added following and started with maps

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
-from flask import Blueprint, render_template, flash, Flask, render_template
+from flask import Blueprint, render_template, flash, jsonify, request, redirect, url_for
 from flask_login import login_required, current_user
 
+from app import db
+from app.models import User, Post, Follow
+from app.src.utils import get_coordinates_of_posts
+
 main = Blueprint('main', __name__)
-mapViewer = Flask(__name__)
 
 
 @main.route('/')  # home page that return 'index'
@@ -13,10 +16,58 @@ def index():
     return page
 
 
-@main.route('/profile')
+@main.route('/profile', methods=['GET', 'POST'])
 def profile():
-    return render_template('profile.html')
+    if request.method == 'GET':
+        return render_template('profile.html')
+    # else:
+    #     username
+    #     email
+    #     public
 
-@main.route('/map')
-def map_func():
-	return render_template('map.html')
+    #     current_user.username = username
+    #     u
+
+    #     db.session.commit()
+
+# api request
+
+
+@main.route('/findFriends', methods=['GET', 'POST'])
+@login_required
+def follow():
+    if request.method == 'GET':
+        return render_template('follow.html')
+    else:
+        usernameToFollow = request.form.get('username')
+
+        # check if username to follow is in database and then follow
+        user = User.query.filter_by(username=usernameToFollow).first()
+
+        if not user:
+            flash('User not found!', 'danger')
+            return redirect(url_for('main.follow'))
+
+        follow = Follow(user_id=current_user.id, following_id=user.id)
+
+        # Add the follow relationship to the db
+        db.session.add(follow)
+        db.session.commit()
+
+        flash(f'You are now following {user.username}!', 'success')
+        return redirect(url_for('main.follow'))
+
+
+@main.route('/map/get_locations', methods=['GET', 'POST'])
+def get_locations():
+    if request.method == 'GET':
+        # return render_template('/map/map.html')
+        return render_template('map/maptest.html')
+    elif request.method == 'POST':
+        # query only post whose users are public
+        posts = Post.query.all()
+
+        # return in json format
+        coordinates = get_coordinates_of_posts(posts)
+
+        return jsonify(coordinates)
--- app/src/utils.py
+++ app/src/utils.py
@@ -23,3 +23,16 @@ def savePostImage(form_picture):
         return False
 
     return pic_fn
+
+
+def get_coordinates_of_posts(posts):
+    coordinates = []
+
+    for post in posts:
+        coord = {}
+        coord["longitude"] = post.longitude
+        coord["latitude"] = post.latitude
+
+        coordinates.append(coord)
+    
+    return coordinates
