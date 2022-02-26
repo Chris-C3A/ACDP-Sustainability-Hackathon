f2e59e1c2d2a5d03970c50d88d008ae8ee1223ec
u1trabot <juicyninja11@gmail.com>
2022-02-26 13:24:01 +0000

    Shortened line and improved SQL ready code

--- app/main.py
+++ app/main.py
@@ -6,8 +6,7 @@ main = Blueprint('main', __name__)
 
 @main.route('/')  # home page that return 'index'
 def index():
-    posts = [(1, "My Friday", '/static/stock1.jpg', 'LOL xD'), (2, "Your Monday", '/static/stock2.jpg', 'Funny Caption'), (3, "For the gram!",
-                                                                                                                           '/static/stock3.jpg', 'haha'), (4, "Honestly", '/static/stock4.jpg', 'WasteWatchers Unite!')]  # map(tuple,fetchall(QUERY))
+    posts = [(1, "My Friday", '/static/stock1.jpg', 'LOL xD'), (2, "Your Monday", '/static/stock2.jpg', 'Funny Caption'), (3, "For the gram!",'/static/stock3.jpg', 'haha'), (4, "Honestly", '/static/stock4.jpg', 'WasteWatchers Unite!')]  # map(tuple,sorted(fetchall(QUERY),key=lambda l:datetime.strptime(l[DATE_INDEX]))))
     page = render_template('postGen.html', posts=posts)
     return page
 
