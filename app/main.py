c24f7cbfff0b44ba6c04d6912f5f332f837dce8c
u1trabot <juicyninja11@gmail.com>
2022-02-26 11:58:42 +0000

    Basic Post Page Gen From Output

--- app/main.py
+++ app/main.py
@@ -5,8 +5,13 @@ main = Blueprint('main', __name__)
 
 
 @main.route('/')  # home page that return 'index'
-def index():
-    return render_template('index.html')
+def posts():
+    posts=[(1,"Title1",'/static/avacad.jpg','LOL xD'),(2,"Title2",'/static/avacad.jpg', 'Funny Caption'),(3,"Title3",'/static/avacad.jpg', 'haha'),(4,"Title4",'/static/avacad.jpg','WasteWatchers Unite!')] #map(tuple,fetchall(QUERY))
+    page = render_template('top.html')
+    for post in posts:
+        page += render_template('postGen.html', postID=post[0], title=post[1], imgUrl=post[2], caption=post[3])
+    page += render_template('bottom.html')
+    return page
 
 
 @main.route('/profile')
